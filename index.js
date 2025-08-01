8646a07e3e2346d9b94001c1342783907d6c27d9
Contrero18 <buboacajustin_18@yahoo.com>
2025-08-01 18:36:43 +0300

    Added games

---
+++ index.js
@@ -0,0 +1,12 @@
+function togglePopup1(){
+    document.getElementById("popup1").classList.toggle("active");
+    }
+    function togglePopup2(){
+    document.getElementById("popup2").classList.toggle("active");
+    }
+    function togglePopup3(){
+    document.getElementById("popup3").classList.toggle("active");
+    }
+    function togglePopup4(){
+    document.getElementById("popup4").classList.toggle("active");
+    }
